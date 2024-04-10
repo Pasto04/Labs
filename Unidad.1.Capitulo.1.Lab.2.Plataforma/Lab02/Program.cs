51051c05b1ddc7c52bd81a575f187179aba5edbd
Pasto04 <juanjosepastorino@gmail.com>
2024-04-09 21:00:58 -0300

    Cambio 2

--- Unidad.1.Capitulo.1.Lab.2.Plataforma/Lab02/Program.cs
+++ Unidad.1.Capitulo.1.Lab.2.Plataforma/Lab02/Program.cs
@@ -10,7 +10,15 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-
+            string valor1 = "Este es el valor 1";
+            int valor2 = 5;
+            string valor3 = valor1;
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            Console.WriteLine(valor3);
+            Console.WriteLine();
+            Console.WriteLine("Ingrese una op");
+            Console.ReadKey();
         }
     }
 }
