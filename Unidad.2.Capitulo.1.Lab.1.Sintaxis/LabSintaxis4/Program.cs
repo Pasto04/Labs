913694acc2676b28ab841d3de3a40dc18de18797
Pasto04 <juanjosepastorino@gmail.com>
2024-04-10 20:00:29 -0300

    Cambio 7

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "Juan", "Lourdes", "Pastorino", "Miguel Angel", "Isabella" };
+            int cantItera = 5;
+            for (int i = 0; i < cantItera; i++) {
+                Console.WriteLine(array[cantItera -1 -i]);
+                Console.WriteLine();}
+            Console.ReadKey();
+        }
+    }
+}
