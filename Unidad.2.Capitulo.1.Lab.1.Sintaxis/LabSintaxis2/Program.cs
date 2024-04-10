638f50f1f929743ff95a68a04e39dd9b2097f4f0
Pasto04 <juanjosepastorino@gmail.com>
2024-04-09 21:44:31 -0300

    Cambio 4

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una frase");
+            string inputTexto = Console.ReadLine();
+            if (inputTexto.Length <= 0 )
+            {
+                Console.WriteLine("No se ingreso Texto");
+            }
+            Console.WriteLine("1- " + inputTexto.ToUpper());
+            Console.WriteLine ("2- " + inputTexto .ToLower());
+            Console.WriteLine("3- " + inputTexto.Length);
+            Console.WriteLine("Elija la opcion que desee");
+            ConsoleKeyInfo op = Console.ReadKey();
+            Console.ReadKey();
+            if (op.Key == ConsoleKey.D1)
+            {
+                Console.WriteLine();
+                Console.WriteLine(inputTexto.ToUpper());
+            }
+            if (op.Key == ConsoleKey.D2)  
+            {
+                Console.WriteLine();
+                Console.WriteLine (inputTexto.ToLower());
+            }
+            if(op.Key == ConsoleKey.D3)
+            {
+                Console.WriteLine();
+                Console.WriteLine(inputTexto.Length);
+            }
+        }
+    }
+}
